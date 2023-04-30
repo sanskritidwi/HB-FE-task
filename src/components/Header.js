b37f03e04dcb179b345d7a2dd7e5476a302c2cc7
sanskritidwi <40851223+sanskritidwi@users.noreply.github.com>
2023-04-30 13:21:53 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ function Header(){
     return(
         <>
             <div className='header'>
-<div className='h-con'>HungerBox</div>
-<div className='h-con'>Front End Coding Task</div>
+<div className='h-con'>BuzzBook</div>
+<div className='h-con'></div>
 <div className='h-con'><a href='https://github.com/sanskritidwi/HB-FE-task/tree/master' target="_blank"><button  style={{display:'flex', width:'max-content'}}>Code </button></a></div>
 
             </div>
